7d81eaca9579ed084fb1622dcd1f4094d241e993
Sri Kumbham <srithetree156@gmail.com>
2022-02-15 14:00:29 -0600

    Initial Commit

---
+++ VIBRUNCE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VIBRUNCE
+//
+//  Created by Sri Kumbham on 2/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VIBRUNCE/VIBRUNCEApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIBRUNCEApp.swift
+//  VIBRUNCE
+//
+//  Created by Sri Kumbham on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct VIBRUNCEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
