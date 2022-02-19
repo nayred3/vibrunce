cfbfbb1a35c68fb3e301a767a2a51464bf61f8d7
Sri Kumbham <srithetree156@gmail.com>
2022-02-18 19:29:01 -0600

    basically entire project minus having to make stuff look nice + annotations

--- VIBRUNCE/ContentView.swift
+++ VIBRUNCE/ContentView.swift
@@ -2,15 +2,40 @@
 //  ContentView.swift
 //  VIBRUNCE
 //
-//  Created by Sri Kumbham on 2/15/22.
+//  Created by Sri Kumbham on 2/23/22.
 //
 
+
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = ContentViewModel()
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+            Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+                .navigationTitle("Runner Location")
+                .ignoresSafeArea()
+                .accentColor(Color(.systemPink))
+                .onAppear {
+                    viewModel.checkIfLocationServicesIsEnabled()
+                }
+                NavigationLink(destination: Text("Group Averages \n \n Pace: 8:20 \n \n Total Distance: 3.4 \n \n Elevation Change: 20 Feet \n \n Top Speed: 7 MPH \n \n Calories Burnt: 383")
+                                .bold()
+                                .lineLimit(nil)
+                                .font(.system(size: 27.0)), label: {
+                    Text("Group Running Stats")
+                        .bold()
+                        .background(Color.teal)
+                        .foregroundColor(.black)
+                        .cornerRadius(10)
+                })
+            }
+        }
     }
 }
 
@@ -19,3 +44,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
---
+++ VIBRUNCE/ContentViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  ContentViewModel.swift
+//  VIBRUNCE
+//
+//  Created by Sri Kumbham on 2/18/22.
+//
+
+import MapKit
+
+enum MapDetails {
+    static let startingLocation = CLLocationCoordinate2D(latitude: 29.7512, longitude: -95.3588)
+    static let defaultSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+}
+
+final class ContentViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    @Published var region = MKCoordinateRegion(center: MapDetails.startingLocation,
+                                               span: MapDetails.defaultSpan )
+    
+    var locationManager: CLLocationManager?
+    
+    func checkIfLocationServicesIsEnabled() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+            locationManager?.activityType = CLActivityType.fitness
+            
+        } else {
+            print("Turn location services on")
+        }
+    }
+    
+  private func checkLocationAuthorization() {
+        guard let locationManager = locationManager else {return}
+        
+        switch locationManager.authorizationStatus {
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Location is restricted likely due to privacy settings")
+        case .denied:
+            print("Location was denied. Must be switched on in device settings")
+        case .authorizedAlways, .authorizedWhenInUse:
+            region = MKCoordinateRegion(center: locationManager.location!.coordinate,
+                                        span: MapDetails.defaultSpan)
+        @unknown default:
+            break
+        }
+    }
+    
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+}
